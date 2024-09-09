6b2deda9778a344a8d217e95e5f1d66f8047db9b
MANVEERSINGH <singhmanveer645@gmail.com>
2024-09-09 13:30:31 +0530

    updated user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,6 @@ import express from "express";
 import {isLoggedIn} from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/multer.middleware.js"
 const router = express.Router();
-console.log('lele swad');
 import { register, login, logout, getProfile, forgotPassword, resetPassword, changePassword, updateUser, deleteUser} from "../controllers/userController.js";
 router.post('/register', upload.single('avatar') , register);
 router.post('/login', login);
@@ -13,7 +12,5 @@ router.post('/reset-password', resetPassword);
 router.post('/change-password', isLoggedIn, changePassword);
 router.put('/update-user', isLoggedIn,upload.single('avatar'), updateUser);
 router.delete('/delete-user', deleteUser);
-
-
 
 export default router;
